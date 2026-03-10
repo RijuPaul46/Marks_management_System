b01df33daa68bffabe7a6933a70368f8fa86247b
RIJU PAUL <linearpaul2024@gmail.com>
2026-03-10 15:22:45 +0530

    Refactor student marks display and update logic

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import csv
+
 def display_students():
     with open("marks.csv", "r") as file:
         reader = csv.DictReader(file)
+        print("\nStudent Records\n")
         for row in reader:
             print("Roll No:", row["roll_no"])
             print("Name:", row["name"])
@@ -11,36 +13,56 @@ def display_students():
             print("Total:", row["total"])
             print("----------------------")
 
-display_students()
 def update_mark(subject):
-    students=[]
-    with open("marks.csv","r") as file:
-        reader=csv.DictReader(file)
+    students = []
+
+    with open("marks.csv", "r") as file:
+        reader = csv.DictReader(file)
         for row in reader:
             students.append(row)
-    roll=input("Enter roll number to update the mark")
-    updated_mark=input("Enter the number which will be updated:")
+
+    roll = input("Enter roll number to update marks: ")
+    updated_mark = input("Enter new marks: ")
+
+    found = False
+
     for student in students:
-        if(student["roll_no"]==roll):
-            student[subject]=updated_mark
+        if student["roll_no"] == roll:
+            student[subject] = updated_mark
+            total = int(student["physics"]) + int(student["chemistry"]) + int(student["math"])
+            student["total"] = str(total)
+            found = True
             print("Marks Updated Successfully")
-    with open("marks.csv","w",newline="") as file:
-        fieldnm=["roll_no","name","physics","chemistry","math","total"]
-        writer=csv.DictWriter(file,fieldnames=fieldnm)
+
+    if not found:
+        print("Student not found")
+
+    with open("marks.csv", "w", newline="") as file:
+        fieldnames = ["roll_no", "name", "physics", "chemistry", "math", "total"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(students)
+
 print("Login as Subject Teacher")
-choice=input("Enter :\n 1 for Physics \n 2 for Chemistry \n 3 for Math ")
-subject_map={
-    "1":"physics",
-    "2":"chemistry",
-    "3":"math"
+
+choice = input("""
+Enter:
+1 for Physics
+2 for Chemistry
+3 for Math
+""")
+
+subject_map = {
+    "1": "physics",
+    "2": "chemistry",
+    "3": "math"
 }
+
 if choice in subject_map:
-    subject=subject_map[choice]
+    subject = subject_map[choice]
     display_students()
     update_mark(subject)
-else :
+    print("\nUpdated Records\n")
+    display_students()
+else:
     print("Invalid Choice")
-display_students()
-
